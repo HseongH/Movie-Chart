10ccbd0beb2a574513a03ed293708490c53abc92
unknown <hh4518@gmail.com>
2021-05-03 00:21:31 +0900

    server work done

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, jsonify, request, send_from_directory
 import requests
 import json
 from crawling import currently_being_screened, works_to_be_screend
-from get-movie-list import get-movie-list
+from search import get_search_result
 
 app = Flask(__name__)
 
@@ -17,7 +17,7 @@ def homework():
 def view_result():
     query_receive = request.args.get('query')
 
-    movie_list = get_movie_list(query_receive)
+    movie_list = get_search_result(query_receive)
     
     return jsonify({ 'list': movie_list })
 
@@ -27,6 +27,13 @@ def current():
         cur_screen = json.load(f)
         
     return jsonify({ 'list': cur_screen })
+
+@app.route('/works', methods=['GET'])
+def works():
+    with open('works-to-be-screend.json', 'r', encoding='utf-8') as f:
+        works_screen = json.load(f)
+        
+    return jsonify({ 'list': works_screen })
 
 
 if __name__ == '__main__':
